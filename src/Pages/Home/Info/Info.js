9d79c52756b42ac5be4cb5c6a3d72bc0551edd81
tanverrana <tanversobur@gmail.com>
2022-05-10 15:43:52 +0600

    add card

---
+++ src/Pages/Home/Info/Info.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import InfoCard from '../../InfoCard/InfoCard';
+import clock from "../../../assets/icons/clock.svg";
+import marker from "../../../assets/icons/marker.svg";
+import phone from "../../../assets/icons/phone.svg";
+
+const Info = () => {
+    return (
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 px-20">
+            <InfoCard cardTitle="Opening Hours" bgClass="bg-gradient-to-r from-secondary to-primary" img={marker}></InfoCard>
+            <InfoCard cardTitle="Visit Our Location" bgClass="bg-accent" img={clock}></InfoCard>
+            <InfoCard cardTitle="Contact Us Now" bgClass="bg-gradient-to-r from-secondary to-primary" img={phone}></InfoCard>
+        </div>
+    );
+};
+
+export default Info;
