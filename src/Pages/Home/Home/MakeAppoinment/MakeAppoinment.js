9d2f899b90f1fee743e55fda517f85f9ec7da910
tanverrana <tanversobur@gmail.com>
2022-05-10 20:19:01 +0600

    some style changes and responsive

--- src/Pages/Home/Home/MakeAppoinment/MakeAppoinment.js
+++ src/Pages/Home/Home/MakeAppoinment/MakeAppoinment.js
@@ -12,10 +12,10 @@ const MakeAppoinment = () => {
             <div className="flex-1 hidden lg:block">
                 <img className="mt-[-130px]" src={doctor} alt="" />
             </div>
-            <div className="flex-1">
+            <div className="flex-1 p-7">
                 <h3 className="text-xl text-primary font-bold">Appointment</h3>
                 <h2 className="text-4xl text-white">Make an appointment Today</h2>
-                <p className="text-white">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+                <p className="text-white mb-10">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
                 <PrimaryButton>Get Started</PrimaryButton>
 
             </div>
