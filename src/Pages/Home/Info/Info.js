e9b2addd25b0f254db52bf4b8420bad46212d17f
tanverrana <tanversobur@gmail.com>
2022-05-10 19:35:07 +0600

    info style change

--- src/Pages/Home/Info/Info.js
+++ src/Pages/Home/Info/Info.js
@@ -7,9 +7,9 @@ import phone from "../../../assets/icons/phone.svg";
 const Info = () => {
     return (
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
-            <InfoCard cardTitle="Opening Hours" bgclass="bg-gradient-to-r from-secondary to-primary" img={marker}></InfoCard>
-            <InfoCard cardTitle="Visit Our Location" bgclass="bg-accent" img={clock}></InfoCard>
-            <InfoCard cardTitle="Contact Us Now" bgclass="bg-gradient-to-r from-secondary to-primary" img={phone}></InfoCard>
+            <InfoCard cardTitle="Opening Hours" bgClass="bg-gradient-to-r from-secondary to-primary" img={marker}></InfoCard>
+            <InfoCard cardTitle="Visit Our Location" bgClass="bg-accent" img={clock}></InfoCard>
+            <InfoCard cardTitle="Contact Us Now" bgClass="bg-gradient-to-r from-secondary to-primary" img={phone}></InfoCard>
         </div>
     );
 };
