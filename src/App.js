d5382cf9955e7c36ee006f21b714acf3dadb66d8
tanverrana <tanversobur@gmail.com>
2022-05-10 15:01:33 +0600

    route create

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './Pages/Shared/Navbar';
 import { Routes, Route, Link } from "react-router-dom";
 import Home from './Pages/Home/Home/Home';
 import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
@@ -10,7 +11,9 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
         <Route path="/about" element={<About></About>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
       </Routes>
     </div>
   );
