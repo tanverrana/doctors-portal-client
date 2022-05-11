aa7a3047b8d30c687b411126a604491762d000cb
tanverrana <tanversobur@gmail.com>
2022-05-11 09:19:30 +0600

    dynamic modal

--- src/Pages/Appointment/AvailableAppoinments/AvailableAppoinments.js
+++ src/Pages/Appointment/AvailableAppoinments/AvailableAppoinments.js
@@ -26,7 +26,11 @@ const AvailableAppoinments = ({ date }) => {
                     ></Service>)
                 }
             </div>
-            {treatment && <BookingModal treatment={treatment}></BookingModal>}
+            {treatment && <BookingModal
+                date={date}
+                treatment={treatment}
+                setTreatment={setTreatment}
+            ></BookingModal>}
         </div>
     );
 };
