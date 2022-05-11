ef97c9accf774e80f992085179c3fc09ff9fbc04
tanverrana <tanversobur@gmail.com>
2022-05-11 15:15:29 +0600

    load data from loclhost with mongodb

--- src/Pages/Appointment/AvailableAppoinments/AvailableAppoinments.js
+++ src/Pages/Appointment/AvailableAppoinments/AvailableAppoinments.js
@@ -10,7 +10,7 @@ const AvailableAppoinments = ({ date }) => {
     const [treatment, setTreatment] = useState(null);
 
     useEffect(() => {
-        fetch("services.json")
+        fetch("http://localhost:5000/service")
             .then(res => res.json())
             .then(data => setServices(data))
     }, [])
