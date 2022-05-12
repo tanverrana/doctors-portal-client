424272daede78eed8df1fdded1dc182b179b4d8a
tanverrana <tanversobur@gmail.com>
2022-05-12 22:22:35 +0600

    create user and navigate

--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import auth from '../../firebase.init';
-import { useCreateUserWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useSignInWithGoogle, useUpdateProfile } from 'react-firebase-hooks/auth';
 import { useForm } from "react-hook-form";
 import Loading from '../Shared/Loading';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
     const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
@@ -14,21 +14,28 @@ const SignUp = () => {
         loading,
         error,
     ] = useCreateUserWithEmailAndPassword(auth);
+
+    const [updateProfile, updating, UpdateError] = useUpdateProfile(auth);
+
+    const navigate = useNavigate();
+
     let signInErrorMessage;
 
     if (user || gUser) {
         console.log(user || gUser);
     }
-    if (loading || gLoading) {
+    if (loading || gLoading || updating) {
         return <Loading></Loading>
     }
-    if (error || gError) {
-        signInErrorMessage = <p className="text-red-600">{error?.message || gError?.message}</p>
+    if (error || gError || UpdateError) {
+        signInErrorMessage = <p className="text-red-600">{error?.message || gError?.message || UpdateError?.message}</p>
     }
 
-    const onSubmit = data => {
-        console.log(data);
-        createUserWithEmailAndPassword(data.email, data.password);
+    const onSubmit = async data => {
+        await createUserWithEmailAndPassword(data.email, data.password);
+        await updateProfile({ displayName: data.name });
+        console.log("update done");
+        navigate("/appointment");
     };
     return (
         <div className="flex justify-center items-center h-screen">
