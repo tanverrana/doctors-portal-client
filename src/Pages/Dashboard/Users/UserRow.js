d35445da8fff492cc1a6435373d65fd9d6409b2a
tanverrana <tanversobur@gmail.com>
2022-05-18 01:21:59 +0600

    can not make admin without admin role

--- src/Pages/Dashboard/Users/UserRow.js
+++ src/Pages/Dashboard/Users/UserRow.js
@@ -10,10 +10,17 @@ const UserRow = ({ user, refetch }) => {
                 authorization: `Bearer ${localStorage.getItem("accessToken")}`
             }
         })
-            .then(res => res.json())
+            .then(res => {
+                if (res.status === 403) {
+                    toast.error("Failed to make an admin")
+                }
+                return res.json()
+            })
             .then(data => {
-                refetch();
-                toast.success(`successfully made an admin`);
+                if (data.modifiedCount > 0) {
+                    refetch();
+                    toast.success(`successfully made an admin`);
+                }
             })
     }
     return (
