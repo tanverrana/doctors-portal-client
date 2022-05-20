1d16ef2f77401ca7cbafeceb5714c33e1d59db3e
tanverrana <tanversobur@gmail.com>
2022-05-20 15:42:50 +0600

    show doctors information

--- src/Pages/Dashboard/ManageDoctor/ManageDoctor.js
+++ src/Pages/Dashboard/ManageDoctor/ManageDoctor.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 import Loading from '../../Shared/Loading';
+import DoctorRow from './DoctorRow';
 
 const ManageDoctor = () => {
     const { data: doctors, isLoading } = useQuery("doctors", () => fetch("http://localhost:5000/doctor", {
@@ -15,6 +16,28 @@ const ManageDoctor = () => {
     return (
         <div>
             <h2 className="text-2xl">Manage Doctor:{doctors.length}</h2>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Avatar</th>
+                            <th>Name</th>
+                            <th>Specialty</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            doctors.map((doctor, index) => <DoctorRow
+                                key={doctor._id}
+                                doctor={doctor}
+                                index={index}
+                            ></DoctorRow>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
