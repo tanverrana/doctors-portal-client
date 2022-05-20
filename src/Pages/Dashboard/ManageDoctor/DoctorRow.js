960c3fe9bd23318138d7af66b7f7260f454a3db0
tanverrana <tanversobur@gmail.com>
2022-05-20 23:25:39 +0600

    handle delete doctor button

--- src/Pages/Dashboard/ManageDoctor/DoctorRow.js
+++ src/Pages/Dashboard/ManageDoctor/DoctorRow.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
-const DoctorRow = ({ doctor, index }) => {
-    const { name, specialty, img } = doctor;
+const DoctorRow = ({ doctor, index, refetch }) => {
+    const { name, specialty, img, email } = doctor;
+
+    const handleDelete = email => {
+        fetch(`http://localhost:5000/doctor/${email}`, {
+            method: "DELETE",
+            headers: {
+                authorization: `Bearer ${localStorage.getItem("accessToken")}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.deletedCount) {
+                    toast.success(`Doctor:${name} is deleted.`)
+                    refetch();
+                }
+            })
+    }
     return (
         <tr>
             <th>{index + 1}</th>
@@ -14,7 +32,7 @@ const DoctorRow = ({ doctor, index }) => {
             </td>
             <td>{name}</td>
             <td>{specialty}</td>
-            <td><button class="btn btn-xs btn-error">Delete</button></td>
+            <td><button onClick={() => handleDelete(email)} class="btn btn-xs btn-error">Delete</button></td>
         </tr>
     );
 };
