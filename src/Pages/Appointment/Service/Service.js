abc3faa75db009efcadef737f12b50239b6b8ba7
tanverrana <tanversobur@gmail.com>
2022-05-21 09:48:13 +0600

    service price added with mongo and ui

--- src/Pages/Appointment/Service/Service.js
+++ src/Pages/Appointment/Service/Service.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Service = ({ service, setTreatment }) => {
-    const { name, slots } = service;
+    const { name, slots, price } = service;
     return (
         <div class="card lg:max-w-lg bg-base-100 shadow-xl">
             <div class="card-body">
@@ -13,6 +13,7 @@ const Service = ({ service, setTreatment }) => {
                         : <span className="text-red-500">No Slots available</span>
                 }</p>
                 <p>{slots.length}{slots.length > 1 ? " spaces" : " space"}  available</p>
+                <p><small>Price:${price}</small></p>
                 <div class="card-actions justify-center">
 
                     <label htmlFor="booking-modal"
